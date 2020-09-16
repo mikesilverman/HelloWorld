761f92984ec6b81dd87dffb21844a3b15b3a27a1
mike <tuh04364@temple.edu>
2020-09-16 14:57:29 -0400

    Second commit

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,14 +1,29 @@
 package edu.temple.helloworld;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.myButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text = (TextView)findViewById(R.id.MyTextView);
+                text.setText("You have clicked the button");
+            }
+        });
+
+
+
     }
 }
